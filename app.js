80a307b85a52a756cf1a565075306d553a4e539c
Rory Devane <rory.devane@gmail.com>
2017-04-14 13:22:29 +0100

    moved dev server into seperate file

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
 
-
-new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-    if (err) {
-        console.log(err);
-    }
-
-    console.log('Listening at localhost:3000');
-});
+require('./dev-server')
 
 //html endpoint
 
