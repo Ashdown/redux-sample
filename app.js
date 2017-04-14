dbf7714599c3d856a803c49ef7d1ea70f8bcd827
Rory Devane <rory.devane@gmail.com>
2017-04-14 13:17:59 +0100

    added webpack hot loading

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+}).listen(3000, 'localhost', function (err, result) {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log('Listening at localhost:3000');
+});
 
 //html endpoint
 
@@ -13,4 +29,5 @@ app.get('/test', function(req,res){
     res.send(JSON.stringify({ a: 1 }));
 });
 
-app.listen(3000);
+app.listen(4000);
+
