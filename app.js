44789e1b3a9833e86be75c40e0a769d1224aab07
Rory Devane <rory.devane@gmail.com>
2017-04-14 11:30:03 +0100

    adding basic node app

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//html endpoint
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+//json endpoint
+
+app.get('/test', function(req,res){
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ a: 1 }));
+});
+
+app.listen(3000);
